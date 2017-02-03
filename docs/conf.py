5f026eb2ffcea254baf7e7ab166529e386530dc0
Derek Goddeau <dgodd001@odu.edu>
2017-02-03 14:44:02 -0500

    fix copyright tag

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'antikythera'
-copyright = u'2017, Derek Goddeau'
+copyright = u'2017, Finding Ray'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
