d43bb1c5c4abc98e657db2f31f6b2e8dfa52e67e
Derek Goddeau <dgodd001@odu.edu>
2017-02-04 16:45:22 -0500

    use a terrible hack to make spacing work

--- setup.sh
+++ setup.sh
@@ -40,8 +40,12 @@ shell_setup()
 {
 
     # Do not add repositories each run
-    echo "\n\n[*] Installing without docker\n\n"
+    echo "[*] Installing without docker"
+    echo ""
+    echo ""
     echo "[*] Installing Kivy and Cython dependencies"
+    echo ""
+    echo ""
     apt-get --yes update && apt-get --assume-yes --force-yes install deb-multimedia-keyring
     apt-get --yes update && apt-get --yes install git build-essential
     apt-get --yes install ffmpeg libsdl2-dev libsdl2-image-dev
@@ -50,10 +54,16 @@ shell_setup()
     apt-get --yes install zlib1g-dev
 
     # Install python
-    echo "\n\n[*] Installing python, pip, dev"
+    echo "-------------------------------------------------------------"
+    echo ""
+    echo "[*] Installing python, pip, dev"
+    echo ""
+    echo "-------------------------------------------------------------"
     apt-get --yes install python3 python3-pip python3-dev
         
-    echo "\n\n[*] Installing requirements.txt"
+    echo ""
+    echo ""
+    echo "[*] Installing requirements.txt"
     # Install items in requirements.txt in order from top to bottom
     # This is required because the Cython package must be installed
     # before Kivy and pip provides no way to do this.
