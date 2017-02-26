6f11d3bbc5db00dc6bc3fa94d7a99dcb9e6816b3
Derek Goddeau <dgodd001@odu.edu>
2017-02-26 12:51:47 -0500

    adjust sleep times

--- antikythera/metrics.py
+++ antikythera/metrics.py
@@ -14,19 +14,19 @@ from queue import Queue, Empty
 _logger = logging.getLogger(__name__)
 
 
-def metrics(thread_id, q):
+def metrics(process_id, q):
     """ Perform analyasis of the packets.
 
     """
-    _logger.info("Metrics worker starting on thread {}.".format(thread_id))
+    _logger.info("Metrics worker process {} starting.".format(process_id))
 
     while True:
         try:
             p = q.get(block=True, timeout=10)
-            _logger.debug("Thread {}: Consumed Packet {}: Queue size is now {}.".format(thread_id, p, q.qsize()))
-            sleep(random()+0.7)
+            _logger.debug("Process {} consumed packet {}: Queue size is now {}.".format(process_id, p, q.qsize()))
+            sleep(random()+2)
         except Empty:
-            _logger.info("Thread {}: queue empty.".format(thread_id))
+            _logger.warning("Process {} Queue empty.".format(process_id))
             sleep(1)
 
 if __name__ == "__main__":
