73860021f6d92b3463acf28bf91474f54ed72af6
Derek Goddeau <dgodd001@odu.edu>
2017-02-26 12:50:36 -0500

    add max_qsize and add process names

--- antikythera/antikythera.py
+++ antikythera/antikythera.py
@@ -20,11 +20,12 @@ class anti():
     """ Start the worker processes.
 
     """
-    def __init__(self, num_processes, interface=None, capturefile=None):
+    def __init__(self, num_processes, interface=None, capturefile=None, max_qsize=100000):
         """
 
         """
-        self.queue = Queue()
+        self.MAX_QUEUE_SIZE = max_qsize
+        self.queue = Queue(self.MAX_QUEUE_SIZE)
         self.NUMBER_OF_PROCESSES = num_processes
         self.workers = []
         self.interface = interface
@@ -35,6 +36,7 @@ class anti():
         s = ("Initial Process Manager State:\n" +
              "[*] Queue: {}\n".format(self.queue) +
              "[*] Queue Size: {}\n".format(self.queue.qsize()) +
+             "[*] Max Queue Size: {}\n".format(self.MAX_QUEUE_SIZE) +
              "[*] Number of Processes Initial: {}\n".format(self.NUMBER_OF_PROCESSES) +
              "[*] Number of Processes Created: {}\n".format(len(self.workers)) +
              "[*] Network Interface: {}\n".format(self.interface) +
@@ -48,20 +50,21 @@ class anti():
 
         """
         for i in range(self.NUMBER_OF_PROCESSES):
-            _logger.info("Creating metric worker process".format(i))
-            metric_worker = Process(target=metrics, args=(i + 2, self.queue))
+            name = "metric-" + str(i)
+            _logger.info("Creating metric process: {}".format(name))
+            metric_worker = Process(target=metrics, name=name, args=(name, self.queue))
             self.workers.append(metric_worker)
 
-        _logger.info("Creating radio worker process")
-        radio_worker = Process(target=radio, args=(1, self.queue))
+        _logger.info("Creating radio process: radio")
+        radio_worker = Process(target=radio, name="radio", args=("radio", self.queue))
         self.workers.append(radio_worker)
 
-        #_logger.info("Creating GUI process")
-        #gui_worker = Process(target=display, args=())
+        #_logger.info("Creating GUI process: gui")
+        #gui_worker = Process(target=display, name="gui", args=())
         #self.workers.append(gui_worker)
 
         for worker in self.workers:
-            _logger.info("Starting process".format(worker))
+            _logger.info("Starting process: {}".format(worker))
             worker.start()
 
 
@@ -70,7 +73,7 @@ class anti():
 
         """
         for worker in self.workers:
-            _logger.info("Joining process".format(worker))
+            _logger.info("Joining process: {}".format(worker))
             worker.join()
 
 if __name__ == '__main__':
