2cb042d4d3ca49a0819d6d99290a6881871e0dba
Derek Goddeau <dgodd001@odu.edu>
2017-03-04 18:25:42 -0500

    remove `create_parser()' & add TRACE log level

--- antikythera/cli.py
+++ antikythera/cli.py
@@ -10,11 +10,10 @@ and it will be installed on the system.
 """
 import os
 import sys
-import argparse
 import logging
 
 from antikythera import __version__
-from antikythera.antikythera import anti
+from antikythera.antikythera import anti, create_parser
 
 try:
     import appdirs
@@ -27,6 +26,11 @@ __copyright__ = "Finding Ray"
 __license__ = "GNU GPLv3+"
 
 _logger = logging.getLogger(__name__)
+
+# Define numerical value for a logging level
+# that is one numerical value lower than DEBUG
+logging.TRACE = 9
+
 
 def main(args):
     """Main entry point allowing external calls.
@@ -81,83 +85,6 @@ def main(args):
     logging.shutdown()
 
 
-
-def create_parser():
-    """ Parse command line parameters.
-
-    :return: command line parameters as :obj:`argparse.Namespace`
-    Args:
-        args ([str]): List of strings representing the command line arguments.
-
-    Returns:
-        argparse.Namespace: Simple object with a readable string
-        representation of the argument list.
-
-    """
-    parser = argparse.ArgumentParser(
-        description="IMSI Catcher Detector.")
-    source=parser.add_mutually_exclusive_group()
-    logs=parser.add_mutually_exclusive_group()
-    parser.add_argument(
-        '-t',
-        '--threads',
-        nargs='?',
-        type=int,
-        default=1,
-        dest="threads",
-        help="Number of threads to use.",
-        action='store'),
-    parser.add_argument(
-        '-q',
-        '--qsize',
-        nargs='?',
-        type=int,
-        default=None,
-        dest="qsize",
-        help="The maximum queue size for packets waiting to be processed.",
-        action='store'),
-    parser.add_argument(
-        '--headless',
-        default=False,
-        dest="headless",
-        help="Run in headless mode without GUI.",
-        action='store_true'),
-    logs.add_argument(
-        '-v',
-        '--verbose',
-        dest="loglevel",
-        help="set loglevel to INFO",
-        action='store_const',
-        const=logging.INFO)
-    logs.add_argument(
-        '-vv',
-        '--very-verbose',
-        dest="loglevel",
-        help="set loglevel to DEBUG",
-        action='store_const',
-        const=logging.DEBUG)
-    source.add_argument(
-        '-c',
-        '--capture',
-        nargs='?',
-        type=str,
-        default=None,
-        dest="pcap",
-        help="Path to a capture file to use as input.",
-        action='store'),
-    source.add_argument(
-        '-i',
-        '--interface',
-        nargs='?',
-        type=str,
-        default=None,
-        dest="interface",
-        help="The identifier of the network interface to use.",
-        action='store')
-
-    return parser
-
-
 def setup_logs(loglevel):
     """ Set up logger to be used between all modules.
 
@@ -171,6 +98,7 @@ def setup_logs(loglevel):
             for more information.
 
     """
+    # Setup Logging Directory
     logdir = appdirs.user_log_dir(__name__, __author__)
     if not os.path.exists(logdir):
         try:
@@ -179,6 +107,10 @@ def setup_logs(loglevel):
             print("{}".format(e))
     logfile = logdir + ".txt"
     print("[*] Logfile: {}".format(logfile))
+
+    # Add logging level TRACE
+    logging.addLevelName(logging.TRACE, "TRACE")
+    logging.Logger.trace = trace # Set Logger.trace to the trace function
 
     # Convert `logging' string to a level that can be set
     logging.basicConfig(level=loglevel, filename=logfile, filemode='w')
@@ -196,21 +128,19 @@ def setup_logs(loglevel):
     _logger.addHandler(ch)
 
 
+def trace(self, message, *args, **kws):
+    """ Define trace logging level
+
+    Yes, logger takes its '*args' as 'args'.
+
+    """
+    self._log(logging.TRACE, message, args, **kws) 
+
+
 def run():
     """ Entry point for console_scripts.
 
     """
-    main(sys.argv[1:])
-    return 0
-
-
-def headless():
-    """ Entry point for console_scripts.
-
-    Force headless mode.
-
-    """
-    sys.argv.append('--headless')
     main(sys.argv[1:])
     return 0
 
