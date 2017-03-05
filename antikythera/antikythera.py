548bb437c2da6a60e6f80ec4e51e9708c7adb3d0
Derek Goddeau <dgodd001@odu.edu>
2017-03-04 22:53:07 -0500

    pickle problems

--- antikythera/antikythera.py
+++ antikythera/antikythera.py
@@ -10,9 +10,9 @@ import logging
 import argparse
 import appdirs
 
-from multiprocessing import Manager, Process, Pool, Queue
+from multiprocess import Process, Queue
 
-from antikythera.radio import radio
+from antikythera.capture import Capture
 from antikythera.metrics import metrics
 
 _logger = logging.getLogger(__name__)
@@ -60,17 +60,27 @@ class anti():
         """
         for i in range(self.NUMBER_OF_PROCESSES):
             name = "metric-" + str(i)
-            _logger.info("Creating metric process: {}".format(name))
+            _logger.info("Anti: Creating metric process {}".format(name))
             metric_worker = Process(target=metrics, name=name, daemon=True, args=(name, self.queue))
             self.workers.append(metric_worker)
 
-        _logger.info("Creating radio process: radio")
-        radio_worker = Process(target=radio, name="radio", daemon=True, args=("radio", self.queue))
-        self.workers.append(radio_worker)
+        cap = Capture("radio", self.queue, capturefile=self.capturefile)
+
+        _logger.info("Anti: Creating radio process radio")
+        if self.interface != None:
+            cap_worker = Process(target=cap.capture, name="radio", daemon=True, args=())
+        elif self.capturefile != None:
+            cap_worker = Process(target=cap.capture, name="radio", daemon=True, args=())
+        else:
+            _logger.critical("Anti: no capture method supplied aborting!")
+
+        self.workers.append(cap_worker)
 
         for worker in self.workers:
-            _logger.info("Starting process: {}".format(worker))
+            _logger.info("Anti: Starting process {}".format(worker))
             worker.start()
+
+        _logger.info("Anti: successfully started")
 
 
     def join(self):
@@ -78,7 +88,7 @@ class anti():
 
         """
         for worker in self.workers:
-            _logger.info("Joining process: {}".format(worker))
+            _logger.info("Anti: Joining process {}".format(worker))
             worker.join()
     
 
--- antikythera/metrics.py
+++ antikythera/metrics.py
@@ -18,15 +18,15 @@ def metrics(process_id, q):
     """ Perform analyasis of the packets.
 
     """
-    _logger.info("Metrics worker process {} starting.".format(process_id))
+    _logger.info("Metrics: worker process {} started".format(process_id))
 
     while True:
         try:
-            p = q.get(block=True, timeout=10)
-            _logger.trace("Process {} consumed packet {}: Queue size is now {}.".format(process_id, p, q.qsize()))
-            sleep(random()+2)
+            p = q.get(timeout=10)
+            _logger.trace("Metrics: Process {} consumed packet {} Queue size is now {}".format(process_id, p['gsmtap'].frame_nr, q.qsize()))
+            sleep(1)
         except Empty:
-            _logger.info("Process {} Queue empty.".format(process_id))
+            _logger.info("Metrics: Process {} Queue empty".format(process_id))
             sleep(1)
 
 if __name__ == "__main__":
