b091733c95adbe5364bdca8fa28a1e6da3d21c51
Derek Goddeau <dgodd001@odu.edu>
2017-03-06 00:14:15 -0500

    inherit from multiprocessing Process, & shutdown()

--- antikythera/decoder.py
+++ antikythera/decoder.py
@@ -6,46 +6,59 @@ Unwrap the pyshark packets and put the needed data into the database.
 
 """
 import logging
+import multiprocessing as mp
+
 
 from random import random
 from time import sleep
-from queue import Queue, Empty
+from queue import Empty
+from multiprocessing import Process, Queue
 
 _logger = logging.getLogger(__name__)
 
+try:
+    import sqlitedict
+except ImportError as e:
+    _logger.error("Decoder: {}".format(e))
+    _logger.info("Decoder: Maybe try `pip install -r requirements.txt'")
+    sys.exit(1)
 
-class Decoder():
+
+class Decoder(Process):
     """ Decode and store the packets for analysis.
 
     """
 
-    def __init__(self, process_id, q):
+    def __init__(self, process_id, q, *args, **kwargs):
+        super(Decoder, self).__init__(*args, **kwargs)
         self.process_id = process_id
         self.q = q
+        self.exit = mp.Event()
 
 
-    def decode(self):
+    def run(self):
         """
 
         """
         _logger.debug("{}: Process started successfully".format(self.process_id))
-        while True:
+        while not self.exit.is_set():
             try:
                 packet = self.q.get(timeout=10)
-                _logger.trace("{}: Consumed packet Queue size is now {}".format(self.process_id, self.q.qsize()))
+                _logger.debug("{}: Consumed packet Queue size is now {}".format(self.process_id, self.q.qsize()))
                 self.decode_packet(packet)
                 self.store_packet(packet)
                 sleep(1)
             except Empty:
                 _logger.info("{}: Queue empty".format(self.process_id))
                 sleep(1)
+        _logger.info("{}: Exiting".format(self.process_id))
 
 
     def decode_packet(self, packet):
         """ Get only the needed attributes from the packet.
 
         """
-        _logger.trace("{}: Decoding packet {}".format(self.process_id, packet['gsmtap'].frame_nr))
+        _logger.debug("{}: Decoding packet {}".format(self.process_id, packet['gsmtap'].frame_nr))
         pass
 
 
@@ -53,8 +66,13 @@ class Decoder():
         """ Put packet into database.
 
         """
-        _logger.trace("{}: Storing packet {}".format(self.process_id, packet['gsmtap'].frame_nr))
+        _logger.debug("{}: Storing packet {}".format(self.process_id, packet['gsmtap'].frame_nr))
         pass
+
+
+    def shutdown(self):
+        _logger.info("{}: Recieved shutdown command".format(self.process_id))
+        self.exit.set()
 
 
 if __name__ == "__main__":
