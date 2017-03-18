98260427f28ebde690297163da33634704a57952
Derek Goddeau <dgodd001@odu.edu>
2017-03-18 14:35:49 -0400

    install requirements.txt quiet, install curl

--- install.sh
+++ install.sh
@@ -121,7 +121,7 @@ pip_install()
     echo "[*] Installing antikythera"
 
     # Setup to install
-    ( set -x; $sh_c 'sleep 3; apt-get --yes -qq --show-progress install python3-pip' )
+    ( set -x; $sh_c 'sleep 3; apt-get --yes -qq --show-progress install python3-pip curl' )
     ( set -x; $sh_c 'sleep 3; pip3 -q install --upgrade pip' )
     ( set -x; $sh_c 'sleep 3; pip3 -q install --upgrade setuptools' )
 
@@ -129,7 +129,7 @@ pip_install()
     # It always tries to install kivy before it's cython
     # dependency. `requirements.txt` has the depends listed
     # in order from top to bottom to support this.
-    ( set -x; $sh_c 'sleep 3; curl -sSL "https://gitlab.com/finding-ray/antikythera/raw/master/requirements.txt" | xargs -n 1 -L 1 pip3 install' )
+    ( set -x; $sh_c 'sleep 3; curl -sSL "https://gitlab.com/finding-ray/antikythera/raw/master/requirements.txt" | xargs -n 1 -L 1 pip3 -q install' )
     ( set -x; $sh_c 'sleep 3; pip3 -q install antikythera' )
 }
 
