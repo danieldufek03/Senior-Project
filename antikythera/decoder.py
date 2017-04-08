612ce8edc0c7903380234b717b1d7f3523f8c6a8
Derek Goddeau <datenstrom@protonmail.com>
2017-04-08 13:08:37 -0400

    Enter minimal packet data to SQLite3 database

--- antikythera/decoder.py
+++ antikythera/decoder.py
@@ -6,14 +6,19 @@ Unwrap the pyshark packets and put the needed data into the database.
 
 """
 import logging
+import sqlite3
+import datetime
+import appdirs
+import time
 import multiprocessing as mp
-
 
 from random import random
 from queue import Empty
 from multiprocessing import Process, Queue
 
 _logger = logging.getLogger(__name__)
+
+__author__ = "TeamAwesome"
 
 try:
     import sqlitedict
@@ -34,18 +39,42 @@ class Decoder(Process):
         self.q = q
         self.exit = mp.Event()
 
+        self.datadir = appdirs.user_data_dir(__name__, __author__)
+        self.conn = None
+        self.c = None
+
 
     def run(self):
         """
 
         """
         _logger.debug("{}: Process started successfully".format(self.process_id))
+        _logger.info("{}: Database storage set to {}".format(self.process_id,
+            self.datadir))
+        self.conn = sqlite3.connect(self.datadir, check_same_thread=False)
+        self.c = self.conn.cursor()
+        self.c.execute('''CREATE TABLE IF NOT EXISTS PACKETS(
+            UnixTime REAL,
+            PeopleTime TEXT,
+            TMSI TEXT,
+            IMSI TEXT,
+            LAC TEXT,
+            CID TEXT,
+            ARFCN TEXT,
+            MCC TEXT,
+            MNC TEXT,
+            IMEISV TEXT,
+            FrameNumber TEXT,
+            HASH TEXT PRIMARY KEY
+            )'''
+        )
+        self.conn.close()
         while not self.exit.is_set():
             try:
                 packet = self.q.get(timeout=10)
                 _logger.debug("{}: Consumed packet Queue size is now {}".format(self.process_id, self.q.qsize()))
-                self.decode_packet(packet)
-                self.store_packet(packet)
+                packet_data = self.decode_packet(packet)
+                self.store_packet(packet_data, packet)
             except Empty:
                 _logger.info("{}: Queue empty".format(self.process_id))
         _logger.info("{}: Exiting".format(self.process_id))
@@ -55,23 +84,65 @@ class Decoder(Process):
         """ Get only the needed attributes from the packet.
 
         """
-        _logger.debug("{}: Decoding packet {}".format(self.process_id, packet['gsmtap'].frame_nr))
-        pass
+        self.conn = sqlite3.connect(self.datadir, check_same_thread=False)
+        self.c = self.conn.cursor()
+        _logger.debug("{}: Decoding packet {}".format(self.process_id,
+            packet['gsmtap'].frame_nr))
+        packet_data = {}
+        # Prevent collisions when running multiple times on same capture file
+        packet_data.update({"hash" : hash((packet.__hash__(), time.time()))})
+        packet_data.update({"frame_nr" : float(packet['gsmtap'].frame_nr)})
+        packet_data.update({"timestamp" : float(packet.sniff_timestamp)})
+        packet_data.update({"datetime" :
+            str(datetime.datetime.fromtimestamp(
+                    packet_data['timestamp']).strftime('%Y-%m-%d%H:%M:%S')
+                )})
+        return packet_data
 
 
-    def store_packet(self, packet):
+    def store_packet(self, packet_data, packet):
         """ Put packet into database.
 
         """
-        _logger.debug("{}: Storing packet {}".format(self.process_id, packet['gsmtap'].frame_nr))
-        pass
+        _logger.debug("{}: Storing packet {}".format(self.process_id,
+            packet['gsmtap'].frame_nr))
+        self.c.execute(
+            """INSERT INTO PACKETS(
+                UnixTime,
+                PeopleTime,
+                TMSI,
+                IMSI,
+                LAC,
+                CID,
+                ARFCN,
+                MCC,
+                MNC,
+                IMEISV,
+                FrameNumber,
+                HASH
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """, (
+                packet_data['timestamp'],
+                packet_data['datetime'],
+                '123456789012345',
+                '324345566767899',
+                '303',
+                '151515',
+                '131313',
+                '232',
+                '02',
+                '1234567891234567',
+                packet_data['frame_nr'],
+                packet_data['hash']
+            )
+        )
+        self.conn.commit()
+        self.conn.close()
 
 
     def shutdown(self):
         _logger.info("{}: Recieved shutdown command".format(self.process_id))
         self.exit.set()
-
-
 
 
 if __name__ == "__main__":
