21068ff57a8601b7a265ee704a8f48a52a765162
sarahdudley <sdudl001@odu.edu>
2017-04-08 18:15:28 -0400

    In the process of adding a timeout for database access (currently broken).

--- antikythera/__init__.py
+++ antikythera/__init__.py
@@ -1,6 +1,11 @@
 import pkg_resources
-
+import sqlite3
 try:
-    __version__ = pkg_resources.get_distribution(__name__).version
+    __version__  = pkg_resources.get_distribution(__name__).version
+    _logger      = logging.getLogger(__name__)
+    __author__   = "TeamAwesome"
+    self.datadir = appdirs.user_data_dir(__name__, __author__)
+    
+    self.datadir.configure('PRAGMA busy_timeout', 60000)
 except:
     __version__ = 'unknown'
--- antikythera/metrics.py
+++ antikythera/metrics.py
@@ -6,12 +6,15 @@ Implementation of the metrics that detect IMSI Catchers
 
 """
 import logging
+import sqlite3
+import appdirs
 import multiprocessing as mp
 
 from time import sleep
 from multiprocessing import Process, Queue
 
 _logger = logging.getLogger(__name__)
+__author__ = "TeamAwesome"
 
 class Metrics(Process):
     """ The metrics
@@ -21,17 +24,26 @@ class Metrics(Process):
     def __init__(self, process_id, *args, **kwargs):
         super(Metrics, self).__init__(*args, **kwargs)
         self.process_id = process_id
+        self.datadir = appdirs.user_data_dir(__name__, __author__)
+        self.conn = None
+        self.c = None
         _logger.debug("{}: Process started successfully".format(self.process_id))
         self.exit = mp.Event()
-
 
     def run(self):
         """
 
         """
+        self.conn = sqlite3.connect(self.datadir, check_same_thread=False)
+        self.c = self.conn.cursor()
+
         while not self.exit.is_set():
             _logger.debug("{}: doing metrics stuff".format(self.process_id))
             sleep(3)
+            c.execute("SELECT * FROM PACKETS")  
+            for row in c.fetchall():   
+            	_logger.debug("{}: {}".format(self.process_id, row))
+            self.conn.close()
         _logger.info("{}: Exiting".format(self.process_id))
 
 
