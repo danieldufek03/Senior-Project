a0ea2db2829535e4304db238d630dfba473d1103
Robert McDougal <rmcdo014@odu.edu>
2017-04-15 17:16:28 -0400

    Added function for previous time, cleaned up, started another table

--- antikythera/metrics.py
+++ antikythera/metrics.py
@@ -32,6 +32,7 @@ class Metrics(Process):
         self.packetList = []
         self.areaCidList = []
         self.inconsistentAreaCodeList = []
+        self.tempPreviousTimeList = []
         _logger.debug("{}: Process started successfully".format(self.process_id))
         self.exit = mp.Event()
 
@@ -65,7 +66,7 @@ class Metrics(Process):
             )'''
         )
 
-        self.c.execute('''CREATE TABLE IF NOT EXISTS TEMP_INCONSISTENT_AREA_CODE(
+        self.c.execute('''CREATE TABLE IF NOT EXISTS TEMP_PREVIOUS_TIME(
             LAC TEXT,
             PreviousTime TEXT
             )'''
@@ -84,7 +85,7 @@ class Metrics(Process):
             	_logger.trace("{}: {}".format(self.process_id, row))
             	self.packetList.append(row)
             self.conn.close()
-        
+
         sizeOfPacketList = len(self.packetList)
         _logger.debug("{}: Length of packetList {}".format(self.process_id, sizeOfPacketList))
 
@@ -97,8 +98,6 @@ class Metrics(Process):
     The following sql query will find packets that share the same Location Area Code and Cell ID, but have different frequencies.
     """
     def sameAreaAndCellId(self):
-        #PrevTime = datetime.datetime.now() - datetime.timedelta(days = 730, minutes=5)
-
         self.conn = sqlite3.connect(self.datadir, check_same_thread=False)
         self.c = self.conn.cursor()
 
@@ -109,6 +108,11 @@ class Metrics(Process):
                         GROUP BY LAC, CID
                         HAVING COUNT(*) > 1) B
             ON A.LAC = B.LAC AND A.CID = B.CID AND A.ARFCN <> B.ARFCN""")
+        
+        #self.c.execute("""SELECT LAC, CID, ARFCN FROM B
+         #   WHERE peopletime > 
+         #   (SELECT PreviousTime FROM TEMP_PREVIOUS_TIME)""")
+        
         for row in self.c.fetchall():
             _logger.trace("{}: {}".format(self.process_id, row))
             self.areaCidList.append(row)
@@ -120,33 +124,49 @@ class Metrics(Process):
     The following sql query pull the area code that differs by datetime minus 5 minutes
     and places it into a seperate table INCONSISTENT_AREA_CODE.
     """
+
     def inconsistentAreaCode(self):
-        PrevTime = datetime.datetime.now() - datetime.timedelta(days = 730, minutes=5)
         self.conn = sqlite3.connect(self.datadir, check_same_thread=False)
         self.c = self.conn.cursor()
 
+        self.createTempTable_PreviousTime()
+
         self.c.execute("INSERT INTO INCONSISTENT_AREA_CODE SELECT LAC, PeopleTime FROM PACKETS")
-        self.c.execute(
-            """INSERT INTO TEMP_INCONSISTENT_AREA_CODE(
-            LAC,
-            PreviousTime
-            ) VALUES (?, ?)
-            """, (
-                '757',
-                PrevTime
-                )
-            )
-        #self.c.execute("SELECT * FROM INCON_LAC")
-        #self.c.execute("SELECT * FROM TEMP_INCONSISTENT_AREA_CODE")
         self.c.execute("""SELECT LAC, PeopleTime FROM INCONSISTENT_AREA_CODE
              WHERE PeopleTime > 
-            (SELECT PreviousTime FROM TEMP_INCONSISTENT_AREA_CODE)""")
+            (SELECT PreviousTime FROM TEMP_PREVIOUS_TIME)""")
         for row in self.c.fetchall():
-            _logger.debug("{}: {}".format(self.process_id, row))
+            _logger.trace("{}: {}".format(self.process_id, row))
             self.inconsistentAreaCodeList.append(row)
         sizeOfinconsistentAreaCodeList = len(self.inconsistentAreaCodeList)
         _logger.trace("{}: Length of inconsistentAreaCodeList {}".format(self.process_id, sizeOfinconsistentAreaCodeList))
         self.conn.close()
+
+    """
+    create a temp table that houses local area code and a preivious time stamp for 
+    verification purposes
+    """
+
+    def createTempTable_PreviousTime(self):
+        PrevTime = datetime.datetime.now() - datetime.timedelta(days = 730, minutes=5)
+        self.conn = sqlite3.connect(self.datadir, check_same_thread=False)
+        self.c = self.conn.cursor()
+
+        self.c.execute(
+            """INSERT INTO TEMP_PREVIOUS_TIME(
+            LAC,
+            PreviousTime
+            ) VALUES (?, ?)
+            """, (
+                ("SELECT LAC FROM PACKETS"),
+                PrevTime
+                )
+            )
+        for row in self.c.fetchall():
+            _logger.trace("{}: {}".format(self.process_id, row))
+            self.tempPreviousTimeList.append(row)
+        sizeOftempPreviousTimeList = len(self.tempPreviousTimeList)
+        _logger.trace("{}: Length of tempPreviousTimeList {}".format(self.process_id, sizeOftempPreviousTimeList))
 
     def shutdown(self):
 
