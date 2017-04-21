f219c40c137bed3d41498c5fd7eb14205a438720
fxmartinez <fmart010@odu.edu>
2017-04-21 13:45:14 -0400

    Set numPackets default value to 0.

--- antikythera/antikythera.py
+++ antikythera/antikythera.py
@@ -49,7 +49,7 @@ class Anti(Process):
         #_logger.info(self)
 
         # Allocates shared memory
-        self.sharedMemory = {'numPackets': mp.Value('i', 100), 'numSuspectPackets': mp.Value('i', 0)}
+        self.sharedMemory = {'numPackets': mp.Value('i', 0), 'numSuspectPackets': mp.Value('i', 0)}
 
     def __str__(self):
         s = ("Initial Process Manager State:\n" +
